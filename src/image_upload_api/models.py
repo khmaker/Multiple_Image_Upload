f16ed850ade2a477f2b212e05d866b2bb2a71fdb
Maxim Pikul <maxim.pikul@drivepixels.ru>
2021-09-24 16:06:27 +0300

    Add Article and Image models

--- src/image_upload_api/models.py
+++ src/image_upload_api/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# coding=utf-8
+from django.db.models import (
+    CASCADE, FileField, ForeignKey, Model, TextField,
+)
 
-# Create your models here.
+
+class Article(Model):
+    text = TextField()
+
+
+class Image(Model):
+    image = FileField()
+    article = ForeignKey(
+        'Article',
+        on_delete=CASCADE,
+        related_name='images'
+    )
