94d4450e0924998be282a44cffb10d14e104825b
Maxim Pikul <maxim.pikul@drivepixels.ru>
2021-09-24 16:08:57 +0300

    Add urls

--- src/image_upload_api/urls.py
+++ src/image_upload_api/urls.py
@@ -1,1 +1,15 @@
 # coding=utf-8
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from image_upload_api.views import ArticleViewSet
+
+v1_router = DefaultRouter()
+v1_router.register(
+    'articles',
+    ArticleViewSet,
+    basename='articles'
+    )
+urlpatterns = [
+    path('', include(v1_router.urls)),
+    ]
