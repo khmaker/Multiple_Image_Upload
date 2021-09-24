20168c515aab539b63701ca585c5bc5368acf014
Maxim Pikul <maxim.pikul@drivepixels.ru>
2021-09-24 16:09:14 +0300

    Add ArticleViewSet

--- src/image_upload_api/views.py
+++ src/image_upload_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# coding=utf-8
+from django.views.generic import CreateView, DetailView
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from image_upload_api.models import Article
+from image_upload_api.serializers import ArticleSerializer
+
+
+class ArticleViewSet(ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    parser_classes = (MultiPartParser, FormParser)
+
